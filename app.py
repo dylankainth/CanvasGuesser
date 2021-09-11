d17e4b6e0c906dc864273aff90a349283abff894
Kainth (B-Fifth) Dylan <19KainthD@harrowschool.org.uk>
2021-09-11 10:10:40 +0100

    v0.8

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
+from logging import NullHandler
 from flask import Flask, render_template, send_from_directory, request
 from flask_socketio import SocketIO, join_room, leave_room, send, emit
 from werkzeug import datastructures
 from random import random
+from threading import Event
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 games = [
-    {'users':{'bob':{'score':10,'drawing':False},'james':{'score':10,'drawing':False}},'gamenumber':23, 'gamehostusername':'bob', 'closed':False}
+    {'users':{'bob':{'score':10,'drawing':False},'james':{'score':10,'drawing':False}},'gamenumber':23, 'gamehostusername':'bob', 'closed':False, 'word':''}
 ]
 
 def generateandchecknumber():
@@ -72,8 +74,7 @@ def createpage():
 def gamehost():
         data=request.form.to_dict()
         gamecode = generateandchecknumber()
-        games.append({'users':{},'gamenumber':gamecode, 'gamehostusername':data['hostusername'], 'closed':False})
-        #return render_template('gamehost.html', data=request.form.to_dict())
+        games.append({'users':{},'gamenumber':gamecode, 'gamehostusername':data['hostusername'], 'closed':False, 'drawduration':int(data['drawduration'])})
         return render_template('game.html', data={"gamenumber":str(gamecode),"username":data['hostusername']})
 
 @app.route('/static/<path:path>')
@@ -132,14 +133,52 @@ def startgame(data):
             break;
         else:
             continue;
-    
+
     for user in game['users']:
         emit("message",{'text':user+' is picking a word', 'gamenumber':data['gamenumber'],'username':'Server'}, to=room) 
         emit("wordtransport",{'words':['Cat','Dog','Mouse'],'userdrawing':user}, to=room)
 
+        secondswaittime = game['drawduration']
+
+        global timerover
+        timerover = False
+
+        @socketio.on('wordchosen')
+        def chooseword(data):
+            
+            game['word'] = data['word']
+            game['users'][user]['drawing'] = True
+
+            emit("message",{'text':user+' is drawing', 'gamenumber':data['gamenumber'],'username':'Server'}, to=room)
+
+            for i in range(1,secondswaittime+1):
+                emit("timer",{'time':secondswaittime-i,'gamenumber':data['gamenumber']}, to=room)
+                socketio.sleep(1)
+
+            global timerover
+            timerover = True
+            game['users'][user]['drawing'] = False
+        
+        while timerover==False:
+            socketio.sleep(1)
+
+        emit("wordwas",{'data':game}, to=room)
+    
+    emit('gameover',{'stats':game['users']}, to=room)
+    print(game)
+
 @socketio.on('message')
 def on_message(data):
     emit("message", data, to=data['gamenumber'])
+
+    for game in games:
+        if int(data['gamenumber'])==(game['gamenumber']):  
+            print(data)
+            if (data['text']).lower() == game['word'].lower(): 
+                game['users'][(data['username'])]['score'] = game['users'][(data['username'])]['score'] + 1
+            break;
+        else:
+            continue;
 
 @socketio.on('imagetransport')
 def handle_message(data):
---
+++ readme.md
@@ -0,0 +1,14 @@
+# CanvasGuesser
+### Computer Science Summer Project 2021
+
+![GitHub language count](https://img.shields.io/github/languages/count/DylanK46/CanvasGuesser) 			 ![GitHub repo file count](https://img.shields.io/github/directory-file-count/DylanK46/CanvasGuesser) ![GitHub repo size](https://img.shields.io/github/repo-size/DylanK46/CanvasGuesser) ![GitHub repo size](https://img.shields.io/github/issues/DylanK46/CanvasGuesser) ![GitHub repo size](https://img.shields.io/github/last-commit/DylanK46/CanvasGuesser)
+
+## Introduction
+Dylan Kainth's Computer Science Summer Project
+
+CanvasGuesser is a pictionary game, it's basically a clone of the vastly popular [skribbl.io](https://skribbl.io/)
+
+## Inner Workings
+CanvasGuesser uses a Python Flask webserver, which can be found in app.py;
+It uses Socket.io for websocket transmission, which you can read more about [here](https://socket.io/)
+It uses Vue.js in the frontend html, allowing for real-time DOM updates, read more [here](https://vuejs.org/)
